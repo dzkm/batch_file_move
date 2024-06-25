8ce92c429919a96b90490a75663ae38b25319d06
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-06-24 23:16:32 -0300

    fix: fixed build.py

--- build.py
+++ build.py
@@ -2,12 +2,13 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     'bfm/__main__.py',
+    '--name', 'bfm',
     '--onedir',
     '--windowed',
     '--clean',
-    '--specpath ./build/spec',
-    '--distpath ./build/dist',
-    '--workpath ./build/temp',
-    '--contents-directory resources',
+    '--specpath', "build/spec",
+    '--distpath', "build/dist",
+    '--workpath', "build/temp",
+    '--contents-directory', "resources",
 ])
     
