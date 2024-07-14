2a24af026bafd3f9b64258b0840404127f297862
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-07-13 23:38:38 -0300

    feat: added gui mode detection

--- bfm/app.py
+++ bfm/app.py
@@ -1,12 +1,22 @@
 from common import Args
 import gui
-import menu
 import migration
 from tkinter import messagebox
 
+IS_GUI_MODE = True
+
 
 def main(args: Args):
-    gui()
+    global IS_GUI_MODE
+
+    for key, value in vars(args).items():
+        if value is not None:
+            IS_GUI_MODE = False
+            break
+
+    if IS_GUI_MODE:
+        args = gui.draw()
+
     raise SystemExit(0)
     menu_args = menu.menu(args)
     migration_result = migration.start_migration(menu_args)
